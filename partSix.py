358ab3b2bd7e12894bbcd2ef4cdabc0003ed6323
lachlan-mccallum <lm935@kent.ac.uk>
2024-10-10 13:50:12 +0000

    ticket cost

--- partSix.py
+++ partSix.py
@@ -1,1 +1,9 @@
+age = int(input("how old are you? "))
+s = input("are you a student? ")
 
+if s == "yes" and 12 <= age <= 64:
+    print ("student discount - tickets cost £8")
+elif age < 12 or age > 64:
+    print ("child/senior discount - tickets cost £5")
+else:
+    print("tickets cost £10")
