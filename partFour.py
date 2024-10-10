4176c272d70b27f89f6fd26200375e35afae6555
lachlan-mccallum <lm935@kent.ac.uk>
2024-10-10 13:49:42 +0000

    username and password

--- partFour.py
+++ partFour.py
@@ -1,1 +1,9 @@
+u = input("what is your username? ")
+p = input("what is your password? ")
 
+if u == "lm935" and p == "password":
+    print ("Access granted.")
+else:
+    print ("Access denied")
+
+# my password is not "password"
