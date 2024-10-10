022de73dc6c50e09cf5d96a26cc449be6945792d
lachlan-mccallum <lm935@kent.ac.uk>
2024-10-10 13:48:56 +0000

    number guess

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,12 @@
+import random
 
+secret_number = random.randint(1, 10)
+
+guess = int(input("what is yor guess? "))
+
+if guess > secret_number:
+    print ("Too high")
+elif guess < secret_number:
+    print ("Too low")
+else:
+    print ("Correct")
